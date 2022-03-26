3427b3c9531f4edcf9cafd05fbf36b4b80b88a09
jun <qurugi0347@gmail.com>
2022-03-27 00:08:59 +0900

    Add: 최근 상품 리스트 SortSection 추가

---
+++ src/components/RecentListPage/SortSection.tsx
@@ -0,0 +1,77 @@
+import {useState, useMemo} from "react";
+import styled from "styled-components";
+import {SortType} from "api/sample";
+import {getRecentProductIds} from "util/manageRecentProduct";
+import sortSvg from "svg/common/sort.svg";
+
+const useSortSection = () => {
+  const [sortType, setSortType]: [SortType, any] = useState(SortType.RECENT);
+  const searchCount = useMemo(() => {
+    return getRecentProductIds().length;
+  }, []);
+
+  const component = () => (
+    <SectionWrapper>
+      <SearchCount>조회한 상품 {searchCount}개</SearchCount>
+      {useMemo(() => {
+        let sortTypeText = "";
+        switch (sortType as SortType) {
+          case SortType.RECENT:
+            sortTypeText = "최근 조회 순";
+            break;
+          case SortType.PRICE:
+            sortTypeText = "가격 낮은 순";
+            break;
+          default:
+        }
+        return (
+          <SortTypeButton>
+            <SortIcon src={sortSvg} />
+            <span>{sortTypeText}</span>
+          </SortTypeButton>
+        );
+      }, [sortType])}
+    </SectionWrapper>
+  );
+
+  return {
+    sortType,
+    component,
+  };
+};
+
+const SectionWrapper = styled.div`
+  padding: 0 20px;
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  position: sticky;
+  top: 63px;
+  z-index: 2;
+`;
+
+const SearchCount = styled.div`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  text-align: left;
+  padding: 1px 0;
+`;
+
+const SortTypeButton = styled.button`
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  text-align: right;
+  position: relative;
+  & > span {
+    line-height: 16px;
+    padding: 2px 0;
+    display: inline-block;
+  }
+`;
+
+const SortIcon = styled.img`
+  vertical-align: top;
+`;
+
+export default useSortSection;
--- src/pages/RecentListPage.tsx
+++ src/pages/RecentListPage.tsx
@@ -2,17 +2,19 @@ import React, {useEffect, useState} from "react";
 import BaseLayout from "components/layout/base/BaseLayout";
 import useBrandFilters from "components/RecentListPage/BrandFilters";
 import ProductList from "components/RecentListPage/ProductList";
+import useSortSection from "components/RecentListPage/SortSection";
 import {getBrandFilterdProducts, SortType, IProductData} from "api/sample";
 import {getRecentProductIds} from "util/manageRecentProduct";
 
 const RecentListPage = () => {
   const {selectedToggleList, component: BrandFilters} = useBrandFilters();
+  const {sortType, component: SortSection} = useSortSection();
   const [productData, setProductData]: [IProductData[], any] = useState([]);
 
   const getFilteredProductData = async () => {
     const filterdProduct = await getBrandFilterdProducts({
       brandNames: selectedToggleList,
-      sortType: SortType.PRICE,
+      sortType,
       productIds: getRecentProductIds(),
     });
     setProductData(filterdProduct);
@@ -20,7 +22,7 @@ const RecentListPage = () => {
 
   useEffect(() => {
     getFilteredProductData();
-  }, [selectedToggleList]);
+  }, [selectedToggleList, sortType]);
 
   console.log(productData);
 
@@ -28,6 +30,7 @@ const RecentListPage = () => {
     <BaseLayout>
       <>
         <BrandFilters />
+        <SortSection />
         <ProductList data={productData} />
       </>
     </BaseLayout>
