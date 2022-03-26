9f0c170266b8c2fed71fe872be20f21622e993b7
jun <qurugi0347@gmail.com>
2022-03-26 19:03:37 +0900

    Add: 공통 버튼 Component 추가

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+const BaseButton = styled.button`
+  border: 1px solid var(--gray1);
+  text-align: center;
+
+  &.fill {
+    width: 100%;
+    padding: 14px;
+    border-radius: 12px;
+  }
+  &.small {
+    padding: 7px 11px;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 15px;
+  }
+  &.inverse {
+    background: var(--black);
+    border-color: var(--black);
+    color: var(--white);
+  }
+`;
+
+export default BaseButton;
