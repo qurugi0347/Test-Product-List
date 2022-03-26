3f246101a55b680457cb7a0ca8074546cf0e1b0d
jun <qurugi0347@gmail.com>
2022-03-26 22:04:47 +0900

    Add: BrandFilter로 ProductList 조회

--- src/pages/RecentListPage.tsx
+++ src/pages/RecentListPage.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import BaseLayout from "components/layout/base/BaseLayout";
 import useBrandFilters from "components/RecentListPage/BrandFilters";
+import {getBrandFilterdProducts, SortType, IProductData} from "api/sample";
 
 const RecentListPage = () => {
   const {selectedToggleList, component: BrandFilters} = useBrandFilters();
+  const [productData, setProductData]: [IProductData[], any] = useState([]);
+
+  const getFilteredProductData = async () => {
+    const filterdProduct = await getBrandFilterdProducts(
+      selectedToggleList,
+      SortType.RECENT,
+    );
+    setProductData(filterdProduct);
+  };
+
+  useEffect(() => {
+    getFilteredProductData();
+  }, [selectedToggleList]);
+
+  console.log(productData);
 
   return (
     <BaseLayout>
