64c194ca5b2fa95c9407c25161b7a4a1b761181a
jun <qurugi0347@gmail.com>
2022-03-26 16:33:32 +0900

    Add: 공통 Style 적용

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter} from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/index.css";
 
 ReactDOM.render(
   <React.StrictMode>
