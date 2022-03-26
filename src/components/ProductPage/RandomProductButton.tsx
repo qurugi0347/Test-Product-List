e55a8f7e766dbe54bd34174fcabce5f153170947
jun <qurugi0347@gmail.com>
2022-03-26 19:20:18 +0900

    Modify: ShuffleButton 아이콘 추가

--- src/components/ProductPage/RandomProductButton.tsx
+++ src/components/ProductPage/RandomProductButton.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import BaseButton from "components/ui/button";
+import shuffleSvg from "svg/common/shuffle.svg";
 
 interface RandomProductButtonProps {
   onClickButton: () => void;
@@ -10,13 +11,26 @@ interface RandomProductButtonProps {
 const RandomProductButton = ({onClickButton}: RandomProductButtonProps) => {
   return (
     <RandomButton className="fill" onClick={onClickButton}>
-      랜덤 상품 조회
+      <TextWrapper>
+        <ShuffleIcon src={shuffleSvg} />
+        <span>랜덤 상품 조회</span>
+      </TextWrapper>
     </RandomButton>
   );
 };
 
 const RandomButton = styled(BaseButton)`
   margin: 26px 0;
+  position: relative;
+`;
+
+const TextWrapper = styled.div``;
+
+const ShuffleIcon = styled.img`
+  position: relative;
+  top: 10px;
+  transform: translateY(-50%);
+  margin-right: 6px;
 `;
 
 export default RandomProductButton;
